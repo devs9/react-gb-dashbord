a4b2fb7b4bcc51a931c6749195f2a848d1aaba69
devs9 <devs9@users.noreply.github.com>
2020-07-01 11:28:55 +0000

    deploy: 09dbe15f8356e52d872cdffd1ca12d5dddf075c1

--- precache-manifest.1f6aff83e5da3c3bb4d0d36106f3dfe0.js
+++ precache-manifest.1f6aff83e5da3c3bb4d0d36106f3dfe0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "167c0a5d11cdafc2595f1d0753b13420",
+    "revision": "8f8112f86fa2304e84f492ad6db46e42",
     "url": "/react-gb-dashbord/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gb-dashbord/static/css/2.5e04faac.chunk.css"
   },
   {
-    "revision": "14abf59931c82675f6f7",
+    "revision": "4fed628d2a48a6ea836c",
     "url": "/react-gb-dashbord/static/css/main.d52f70dc.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gb-dashbord/static/js/2.d5c25ab2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "14abf59931c82675f6f7",
-    "url": "/react-gb-dashbord/static/js/main.d87e2288.chunk.js"
+    "revision": "4fed628d2a48a6ea836c",
+    "url": "/react-gb-dashbord/static/js/main.eb710ce6.chunk.js"
   },
   {
     "revision": "98560b1a44e6576ddb2f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gb-dashbord/precache-manifest.d1eecf105458b9b100839e219587a0b8.js"
+  "/react-gb-dashbord/precache-manifest.1f6aff83e5da3c3bb4d0d36106f3dfe0.js"
 );
 
 self.addEventListener('message', (event) => {
