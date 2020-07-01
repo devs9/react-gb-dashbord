71f7e2c21c506cb327bfcb5ff266ec898831f450
devs9 <devs9@users.noreply.github.com>
2020-07-01 15:01:11 +0000

    deploy: 5aa3502acd0f293ba1be29f422c1bb0192e4845a

--- precache-manifest.1f6aff83e5da3c3bb4d0d36106f3dfe0.js
+++ precache-manifest.1f6aff83e5da3c3bb4d0d36106f3dfe0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "97af4153ebcbea0c2ecd4a61c58ef941",
+    "revision": "8f8112f86fa2304e84f492ad6db46e42",
     "url": "/react-gb-dashbord/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gb-dashbord/static/css/2.5e04faac.chunk.css"
   },
   {
-    "revision": "83f8687b1fb482c08cd5",
+    "revision": "4fed628d2a48a6ea836c",
     "url": "/react-gb-dashbord/static/css/main.d52f70dc.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-gb-dashbord/static/js/2.d5c25ab2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "83f8687b1fb482c08cd5",
-    "url": "/react-gb-dashbord/static/js/main.78ba4af0.chunk.js"
+    "revision": "4fed628d2a48a6ea836c",
+    "url": "/react-gb-dashbord/static/js/main.eb710ce6.chunk.js"
   },
   {
     "revision": "98560b1a44e6576ddb2f",
