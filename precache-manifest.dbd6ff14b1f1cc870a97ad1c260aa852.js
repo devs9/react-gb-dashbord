191ac3056119f35f61c934be7abe6f8e59ad6441
devs9 <devs9@users.noreply.github.com>
2020-07-01 18:01:10 +0000

    deploy: 54c90acd7a2dc97bc0fa20660cbbd000ec36efdc

--- precache-manifest.dbd6ff14b1f1cc870a97ad1c260aa852.js
+++ precache-manifest.dbd6ff14b1f1cc870a97ad1c260aa852.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3133f57667bcf9d8e8a00b1a6bca233a",
+    "revision": "6675c539c81315023bbd629d14bfde69",
     "url": "/react-gb-dashbord/index.html"
   },
   {
-    "revision": "32bacc5256e02cfed964",
+    "revision": "c8ad630f1aeb621374c9",
     "url": "/react-gb-dashbord/static/css/2.5e04faac.chunk.css"
   },
   {
-    "revision": "9528702cd27cbdea835f",
+    "revision": "cede65f211d649927eaf",
     "url": "/react-gb-dashbord/static/css/main.d52f70dc.chunk.css"
   },
   {
-    "revision": "32bacc5256e02cfed964",
-    "url": "/react-gb-dashbord/static/js/2.d5c25ab2.chunk.js"
+    "revision": "c8ad630f1aeb621374c9",
+    "url": "/react-gb-dashbord/static/js/2.0eb6ada2.chunk.js"
   },
   {
     "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
-    "url": "/react-gb-dashbord/static/js/2.d5c25ab2.chunk.js.LICENSE.txt"
+    "url": "/react-gb-dashbord/static/js/2.0eb6ada2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9528702cd27cbdea835f",
-    "url": "/react-gb-dashbord/static/js/main.31d7a7c3.chunk.js"
+    "revision": "cede65f211d649927eaf",
+    "url": "/react-gb-dashbord/static/js/main.cbf23096.chunk.js"
   },
   {
     "revision": "98560b1a44e6576ddb2f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gb-dashbord/precache-manifest.06dc063f338e42d025de2d8de033e78d.js"
+  "/react-gb-dashbord/precache-manifest.dbd6ff14b1f1cc870a97ad1c260aa852.js"
 );
 
 self.addEventListener('message', (event) => {
